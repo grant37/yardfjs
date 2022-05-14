40170b9f01ededc56d7d83ac3689136e49949aee
grant37 <grant@lovepopcards.com>
2022-05-14 12:44:21 -0600

    Update tests

--- packages/dataset-core/src/DatasetCore.spec.js
+++ packages/dataset-core/src/DatasetCore.spec.js
@@ -3,25 +3,31 @@ const DataFactory = require('@yardfjs/data-factory').default;
 
 let ds, df;
 
+const INITIAL_GRAPH_COUNT = 3;
+const INITIAL_SUBJECT_COUNT = 3;
+const INITIAL_PREDICATE_COUNT = 3;
+const INITIAL_OBJECT_COUNT = 10;
+const EXAMPLE_NAMESPACE = 'http://example.org/';
+
 // generate a non-empty DatasetCore
 beforeEach(() => {
   ds = new DatasetCore();
   df = new DataFactory();
 
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < INITIAL_GRAPH_COUNT; i++) {
     const graph =
       i === 0
         ? df.defaultGraph()
-        : df.namedNode(`http://example.org/graph${i}`);
-    for (let j = 0; j < 5; j++) {
-      const subject = df.namedNode(`http://example.org/subject${i}`);
-      for (let k = 0; k < 5; k++) {
-        const predicate = df.namedNode(`http://example.org/predicate${i}`);
-        for (let l = 0; l < 10; l++) {
+        : df.namedNode(`${EXAMPLE_NAMESPACE}graph${i}`);
+    for (let j = 0; j < INITIAL_SUBJECT_COUNT; j++) {
+      const subject = df.namedNode(`${EXAMPLE_NAMESPACE}subject${j}`);
+      for (let k = 0; k < INITIAL_PREDICATE_COUNT; k++) {
+        const predicate = df.namedNode(`${EXAMPLE_NAMESPACE}predicate${k}`);
+        for (let l = 0; l < INITIAL_OBJECT_COUNT; l++) {
           let object;
           switch (true) {
             case k % 2 === 0:
-              object = df.namedNode(`http://example.org/object${i}`);
+              object = df.namedNode(`${EXAMPLE_NAMESPACE}object${l}`);
               break;
             case k % 3 === 0:
               object = df.blankNode(`b${i}${j}${k}${l}`);
@@ -52,7 +58,7 @@ describe('has', () => {
   // same test as in add, but whatever
   it('should return true for an existing quad', () => {
     // given a DatasetCore
-    // when a quad is added
+    // when the quad is added to the dataset
     const q = df.quad(
       df.namedNode('match'),
       df.namedNode('by'),
@@ -125,7 +131,7 @@ describe('match', () => {
       df.namedNode('by'),
       df.namedNode('predicate')
     );
-    // when a quad is added
+    // when the quad is added to the dataset
     ds.add(q);
     // then the match method should return a DatasetCore with that quad
     expect(ds.match(null, q.predicate).has(q)).toBe(true);
@@ -138,7 +144,7 @@ describe('match', () => {
       df.namedNode('by'),
       df.namedNode('object')
     );
-    // when a quad is added
+    // when the quad is added to the dataset
     ds.add(q);
     // then the match method should return a DatasetCore with that quad
     expect(ds.match(null, null, df.namedNode('object')).has(q)).toBe(true);
@@ -152,9 +158,168 @@ describe('match', () => {
       df.namedNode('the'),
       df.namedNode('graph')
     );
-    // when a quad is added
+    // when the quad is added to the dataset
     ds.add(q);
     // then the match method should return a DatasetCore with that quad
     expect(ds.match(null, null, null, df.namedNode('graph')).has(q)).toBe(true);
   });
+
+  it('should match by subject and predicate', () => {
+    // given a quad
+    const q = df.quad(
+      df.namedNode('match'),
+      df.namedNode('by'),
+      df.namedNode('subjectandpredicate')
+    );
+    // when the quad is added to the dataset
+    ds.add(q);
+    // then the match method should return a DatasetCore with that quad
+    expect(ds.match(q.subject, q.predicate).has(q)).toBe(true);
+  });
+
+  it('should match by subject and object', () => {
+    // given a quad
+    const q = df.quad(
+      df.namedNode('match'),
+      df.namedNode('by'),
+      df.namedNode('subjectandobject')
+    );
+    // when the quad is added to the dataset
+    ds.add(q);
+    // then the match method should return a DatasetCore with that quad
+    expect(ds.match(q.subject, null, q.object).has(q)).toBe(true);
+  });
+
+  it('should match by subject and graph', () => {
+    // given a quad
+    const q = df.quad(
+      df.namedNode('match'),
+      df.namedNode('by'),
+      df.namedNode('subjectandgraph')
+    );
+    // when the quad is added to the dataset
+    ds.add(q);
+    // then the match method should return a DatasetCore with that quad
+    expect(ds.match(q.subject, null, null, q.graph).has(q)).toBe(true);
+  });
+
+  it('should match by predicate and object', () => {
+    // given a quad
+    const q = df.quad(
+      df.namedNode('match'),
+      df.namedNode('by'),
+      df.namedNode('predicateandobject')
+    );
+    // when the quad is added to the dataset
+    ds.add(q);
+    // then the match method should return a DatasetCore with that quad
+    expect(ds.match(null, q.predicate, q.object).has(q)).toBe(true);
+  });
+
+  it('should match by predicate and graph', () => {
+    // given a quad
+    const q = df.quad(
+      df.namedNode('match'),
+      df.namedNode('by'),
+      df.namedNode('predicateandgraph')
+    );
+    // when the quad is added to the dataset
+    ds.add(q);
+    // then the match method should return a DatasetCore with that quad
+    expect(ds.match(null, q.predicate, null, q.graph).has(q)).toBe(true);
+  });
+
+  it('should match by object and graph', () => {
+    // given a quad
+    const q = df.quad(
+      df.namedNode('match'),
+      df.namedNode('by'),
+      df.namedNode('objectandgraph')
+    );
+    // when the quad is added to the dataset
+    ds.add(q);
+    // then the match method should return a DatasetCore with that quad
+    expect(ds.match(null, null, q.object, q.graph).has(q)).toBe(true);
+  });
+
+  it('should match by subject, predicate and object', () => {
+    // given a quad
+    const q = df.quad(
+      df.namedNode('match'),
+      df.namedNode('by'),
+      df.namedNode('subjectpredicateandobject')
+    );
+    // when the quad is added to the dataset
+    ds.add(q);
+    // then the match method should return a DatasetCore with that quad
+    expect(ds.match(q.subject, q.predicate, q.object).has(q)).toBe(true);
+  });
+
+  it('should match by subject, predicate and graph', () => {
+    // given a quad
+    const q = df.quad(
+      df.namedNode('match'),
+      df.namedNode('by'),
+      df.namedNode('subjectpredicateandgraph')
+    );
+    // when the quad is added to the dataset
+    ds.add(q);
+    // then the match method should return a DatasetCore with that quad
+    expect(ds.match(q.subject, q.predicate, null, q.graph).has(q)).toBe(true);
+  });
+
+  it('should match by subject, object and graph', () => {
+    // given a quad
+    const q = df.quad(
+      df.namedNode('match'),
+      df.namedNode('by'),
+      df.namedNode('subjectobjectandgraph')
+    );
+    // when the quad is added to the dataset
+    ds.add(q);
+    // then the match method should return a DatasetCore with that quad
+    expect(ds.match(q.subject, null, q.object, q.graph).has(q)).toBe(true);
+  });
+
+  it('should match by predicate, object and graph', () => {
+    // given a quad
+    const q = df.quad(
+      df.namedNode('match'),
+      df.namedNode('by'),
+      df.namedNode('predicateobjectandgraph')
+    );
+    // when the quad is added to the dataset
+    ds.add(q);
+    // then the match method should return a DatasetCore with that quad
+    expect(ds.match(null, q.predicate, q.object, q.graph).has(q)).toBe(true);
+  });
+
+  it('should match by subject, predicate, object and graph', () => {
+    // given a quad
+    const q = df.quad(
+      df.namedNode('match'),
+      df.namedNode('by'),
+      df.namedNode('subjectpredicateobjectandgraph')
+    );
+    // when the quad is added to the dataset
+    ds.add(q);
+    // then the match method should return a DatasetCore with that quad
+    expect(ds.match(q.subject, q.predicate, q.object, q.graph).has(q)).toBe(
+      true
+    );
+  });
+
+  it('should return a dataset with the expected size', () => {
+    // given a dataset
+    // when matching against a known term
+    const matchDs = ds.match(
+      df.namedNode(`${EXAMPLE_NAMESPACE}subject${INITIAL_SUBJECT_COUNT - 1}`)
+    );
+    // then the dataset should have the expected size (one matching subject per graph, in this case)
+    expect(matchDs.size).toBe(
+      INITIAL_GRAPH_COUNT * INITIAL_PREDICATE_COUNT * INITIAL_OBJECT_COUNT
+    );
+  });
+
+  // still TODO: will treat terms that can't be found as wildcards :(
 });
